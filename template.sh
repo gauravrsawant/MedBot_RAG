7bb6344ba866d2ce7c795b7283641b5623a71834
gauravrsawant <gauravrsawant1313@gmail.com>
2026-02-28 15:44:26 +0530

    Folder Structure

---
+++ template.sh
@@ -0,0 +1,14 @@
+mkdir -p src
+mkdir -p data
+
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch src/.env
+touch src/setup.py
+touch src/app.py
+touch Notebooks/itter1.py
+touch requirements.txt
+
+
+echo "Folders & Files created successfully!"
